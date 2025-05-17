ee30cdfdc84f0a957a5f3613e68006e2799bd3c2
Barasov-Mehdi <barasovmehdii@gmail.com>
2025-05-17 07:06:37 -0700

    05.17.2025

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 connectDB();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json()); 
